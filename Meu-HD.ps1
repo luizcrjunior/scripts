418f97ee2d2b155a275372a82e8c3949464a99d7
luizcrjunior <62113135+luizcrjunior@users.noreply.github.com>
2020-03-12 14:16:21 -0300

    Verificar espaço livre em disco.
    Verificar espaço livre em disco.

---
+++ Meu-HD.ps1
@@ -0,0 +1,6 @@
+﻿#Exemplo função
+
+function Meu-HD
+{
+Get-WmiObject win32_logicaldisk -Filter DriveType=3 | Select-Object DeviceID, @{'Name' = 'Tamanho (GB)'; 'Expression'={[math]::Truncate($_.size / 1GB)}}, @{'Name'='Espaço Livre (GB)'; 'Expression'={[math]::Truncate($_.freespace / 1GB)}}
+}
